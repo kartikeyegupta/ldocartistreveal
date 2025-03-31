4136c0329e3e38cea05c3cd3428d978eb6d6919f
Kartikeye Gupta <kg330@duke.edu>
2025-03-31 13:39:10 -0400

    april fools finished

--- app/aprilfools/page.tsx
+++ app/aprilfools/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="absolute top-4 left-0 right-0 text-center font-bold text-[#fcd598] text-xl z-20 p-4 font-['Lovecraft'] mb-[5%] glow-text">
+      <div className="absolute top-4 left-0 right-0 text-center font-bold text-[#fcd598] text-xl z-20 p-4 font-['Lovecraft'] mb-[8%] glow-text">
         Look around and click to uncover hidden clues...
       </div>
       <NightShutters />
